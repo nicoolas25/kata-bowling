3da5a6fdd4934b91eac67d9954ab9430573583df
Nicolas ZERMATI <nicoolas25@gmail.com>
2015-02-13 20:42:50 +0100

    Shorter and cryptic version.

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,81 +1,33 @@
 class Line
-  def initialize(rolls_str)
-    @rolls = rolls_str.split('').map { |roll_str| Roll.new(self, roll_str) }
+  def initialize(str)
+    @str = str
+    @rolls = @str.chars.each_with_object([]) do |char, rolls|
+      rolls << (char == 'X' ? 10 : char == '/' ? 10 - rolls[-1] : char.to_i)
+    end
   end
 
   def score
-    rolls_to_count.reduce(0) { |sum, roll| sum + roll.score }
+    @cursor = @total = 0
+    10.times { read_frame }
+    @total
   end
 
-  def rolls_to_count
-    if @rolls[-3].strike?
-      @rolls[0..-3]
-    elsif @rolls[-2].spare?
-      @rolls[0..-2]
-    else
-      @rolls
+  private
+
+  def read_frame
+    read_strike || read_couple
+  end
+
+  def read_strike
+    if @str[@cursor] == 'X'
+      @total += @rolls[@cursor] + @rolls[@cursor + 1] + @rolls[@cursor + 2]
+      @cursor += 1
     end
   end
 
-  def next_to(roll)
-    index = @rolls.index(roll)
-    @rolls[index + 1]
-  end
-
-  def prev_to(roll)
-    index = @rolls.index(roll)
-    @rolls[index - 1] if index > 0
-  end
-end
-
-class Roll
-  def initialize(line, roll_str)
-    @line = line
-    @str = roll_str
-  end
-
-  def score
-    if strike?
-      value + next_value(count: 2)
-    elsif spare?
-      value + next_value(count: 1)
-    else
-      value
-    end
-  end
-
-  def strike?
-    @str == 'X'
-  end
-
-  def spare?
-    @str == '/'
-  end
-
-  def value
-    if strike?
-      10
-    elsif spare?
-      10 - prev_value
-    else
-      @str.to_i
-    end
-  end
-
-  def next_value(count: 1)
-    return 0 if count == 0 || next_roll.nil?
-    next_roll.value + next_roll.next_value(count: count - 1)
-  end
-
-  def prev_value
-    prev_roll.nil? ? 0 : prev_roll.value
-  end
-
-  def next_roll
-    @next_roll ||= @line.next_to(self)
-  end
-
-  def prev_roll
-    @prev_roll ||= @line.prev_to(self)
+  def read_couple
+    @total += @rolls[@cursor] + @rolls[@cursor + 1]
+    @total += @rolls[@cursor + 2] if @str[@cursor + 1] == '/'
+    @cursor += 2
   end
 end
