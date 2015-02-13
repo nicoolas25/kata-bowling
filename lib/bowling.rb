e3bca4fb0e162bd938a9d28c767bbbdb9a7d2048
Nicolas ZERMATI <nicoolas25@gmail.com>
2015-02-13 20:59:50 +0100

    Shorter with aliasing.

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,33 +1,29 @@
 class Line
   def initialize(str)
-    @str = str
-    @rolls = @str.chars.each_with_object([]) do |char, rolls|
+    @rolls = str.chars.each_with_object([]) do |char, rolls|
       rolls << (char == 'X' ? 10 : char == '/' ? 10 - rolls[-1] : char.to_i)
     end
   end
 
   def score
-    @cursor = @total = 0
-    10.times { read_frame }
-    @total
+    @cursor = 0
+    (1..10).map{ read_frame }.reduce(0, &:+)
   end
 
   private
 
   def read_frame
-    read_strike || read_couple
-  end
-
-  def read_strike
-    if @str[@cursor] == 'X'
-      @total += @rolls[@cursor] + @rolls[@cursor + 1] + @rolls[@cursor + 2]
+    first, second, follower = @rolls[@cursor .. @cursor + 2]
+    if first == 10
       @cursor += 1
+      first + second + follower
+    else
+      @cursor += 2
+      if first + second == 10
+        first + second + follower
+      else
+        first + second
+      end
     end
-  end
-
-  def read_couple
-    @total += @rolls[@cursor] + @rolls[@cursor + 1]
-    @total += @rolls[@cursor + 2] if @str[@cursor + 1] == '/'
-    @cursor += 2
   end
 end
